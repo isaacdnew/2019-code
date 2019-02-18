3ec46e5796dcfd6eedfd954a334a5742dfe1527d
isaac <isaac@eyelow>
2019-02-18 16:17:26 -0500

    add print statement

--- src/main/java/frc/robot/commands/ResetDrivetrainEncoders.java
+++ src/main/java/frc/robot/commands/ResetDrivetrainEncoders.java
@@ -32,6 +32,7 @@ public class ResetDrivetrainEncoders extends Command {
 	// Called once after isFinished returns true
 	protected void end() {
 		drivetrain.resetEncoders();
+		System.out.println("Encoders have been reset.");
 	}
 
 	// Called when another command which requires one or more of the same
