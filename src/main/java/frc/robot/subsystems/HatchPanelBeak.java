746e223466325f95d5745d970e88bb0201809959
isaac <isaac@eyelow>
2019-02-18 13:15:27 -0500

    add HatchPanelBeak

---
+++ src/main/java/frc/robot/subsystems/HatchPanelBeak.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import frc.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class HatchPanelBeak extends Subsystem {
+	private VictorSP m;
+
+	public HatchPanelBeak() {
+	}
+
+	public void init() {
+		m = new VictorSP(RobotMap.BEAK_MOTOR);
+		m.setInverted(RobotMap.BEAK_MOTOR_INVERTED);
+	}
+
+	public void move(double pwr) {
+		m.set(pwr);
+	}
+
+	public void stop() {
+		m.stopMotor();
+	}
+
+	public void initDefaultCommand() {
+		// setDefaultCommand(new TeleBeak(this));
+	}
+}
