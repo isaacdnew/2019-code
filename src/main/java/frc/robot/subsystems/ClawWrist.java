8c08bc8507c535b3607e6576f394dc388739af96
isaac <isaac@eyelow>
2019-02-18 13:15:06 -0500

    add ClawWrist

---
+++ src/main/java/frc/robot/subsystems/ClawWrist.java
@@ -0,0 +1,85 @@
+package frc.robot.subsystems;
+
+import frc.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ClawWrist extends Subsystem {
+	private WPI_TalonSRX m;
+
+	private final double P = 0.0;
+	private final double I = 0.0;
+	private final double D = 0.0;
+	private final double F = 0.0;
+
+	private final double revsPerTick = 1.0 / 1024.0;
+	private final double beltRatio = 15.0 / 24.0;
+
+	public enum ClawWristPosition {
+		STOWED, LEVEL, DOWN
+	}
+
+	public ClawWrist() {
+	}
+
+	public void init() {
+		m = new WPI_TalonSRX(RobotMap.BEAK_WRIST_MOTOR);
+		m.setInverted(RobotMap.BEAK_WRIST_MOTOR_INVERTED);
+		m.configSelectedFeedbackSensor(FeedbackDevice.Analog);
+		m.selectProfileSlot(0, 0);
+		m.config_kP(0, P);
+		m.config_kI(0, I);
+		m.config_kD(0, D);
+		m.config_kF(0, F);
+	}
+
+	public void setPosition(ClawWristPosition position) {
+		switch (position) {
+		case STOWED:
+			setPosition(0);
+			break;
+		case LEVEL:
+			setPosition(60);
+			break;
+		case DOWN:
+			setPosition(90);
+			break;
+		}
+	}
+
+	public void setPosition(double degreesFromStowed) {
+		// convert angle into encoder ticks
+		double pulleyAngle = degreesFromStowed / beltRatio;
+		double pulleyRevs = pulleyAngle / 360.0;
+		double ticks = pulleyRevs / revsPerTick;
+
+		// set motor setpoint
+		m.set(ControlMode.Position, ticks);
+	}
+
+	public double getPositionTicks() {
+		return m.getSelectedSensorPosition();
+	}
+
+	public double getPositionRevs() {
+		return m.getSelectedSensorPosition() * revsPerTick * beltRatio;
+	}
+
+	public double getPositionDegrees() {
+		return m.getSelectedSensorPosition() * revsPerTick * beltRatio * 360.0;
+	}
+
+	public void stop() {
+		m.stopMotor();
+	}
+
+	public void initDefaultCommand() {
+	}
+}
