c96d7d475cb4c2f28cf654b43a1dcf5d097fadb9
isaac <isaac@eyelow>
2019-02-19 15:45:48 -0500

    switch motor controllers for beak and beakWrist

--- src/main/java/frc/robot/subsystems/BeakWrist.java
+++ src/main/java/frc/robot/subsystems/BeakWrist.java
@@ -2,21 +2,23 @@ package frc.robot.subsystems;
 
 import frc.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.VictorSP;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class BeakWrist extends Subsystem {
-	private VictorSP m;
+	private WPI_TalonSRX m;
 
 	public BeakWrist() {
 	}
 
 	public void init() {
-		m = new VictorSP(RobotMap.BEAK_WRIST_MOTOR);
+		m = new WPI_TalonSRX(RobotMap.BEAK_WRIST_MOTOR);
 		m.setInverted(RobotMap.BEAK_WRIST_MOTOR_INVERTED);
+		m.configOpenloopRamp(0.5);
 	}
 
 	public void move(double pwr) {
