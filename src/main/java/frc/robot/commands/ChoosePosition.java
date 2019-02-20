a6475259f047fefc1aac4ce44355ead3f0e896c7
isaac <isaac@eyelow>
2019-02-19 23:11:11 -0500

    remove print statements

--- src/main/java/frc/robot/commands/ChoosePosition.java
+++ src/main/java/frc/robot/commands/ChoosePosition.java
@@ -25,7 +25,6 @@ public class ChoosePosition extends Command {
 	protected void execute() {
 		if (SmartDashboard.getBoolean("Beak is front", false)) {
 			clawWrist.setPosition(ClawWristPosition.STOWED);
-			System.out.println("beak is front; stowing");
 		} else {
 			if (clawWrist.getPosition() == ClawWristPosition.LEVEL
 					&& SmartDashboard.getBoolean("Elevator at bottom", false)) {
@@ -34,7 +33,6 @@ public class ChoosePosition extends Command {
 					&& !SmartDashboard.getBoolean("Elevator at bottom", false)) {
 				clawWrist.setPosition(ClawWristPosition.LEVEL);
 			}
-			System.out.println("beak is not front; normal");
 		}
 	}
 
