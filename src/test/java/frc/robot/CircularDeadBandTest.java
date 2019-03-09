7f4ea74e9d59398ec2916cabcb23a69749170991
isaac <isaac@eyelow>
2019-03-09 07:16:16 -0500

    add more tests

--- src/test/java/frc/robot/CircularDeadBandTest.java
+++ src/test/java/frc/robot/CircularDeadBandTest.java
@@ -24,15 +24,27 @@ public class CircularDeadBandTest {
 	}
 
 	@Test
-	public void testVertical() {
-		testXY(0, 0.5, 0, 0.264);
-		testXY(0, -0.5, 0, -0.264);
+	public void testQuadrantsTooBig() {
+		double xIn = 0.8;
+		double yIn = 0.8;
+		double xOut = 0.707;
+		double yOut = 0.707;
+		testXY(xIn, yIn, xOut, yOut); // q1
+		testXY(-xIn, yIn, -xOut, yOut); // q2
+		testXY(-xIn, -yIn, -xOut, -yOut); // q3
+		testXY(xIn, -yIn, xOut, -yOut); // q4
 	}
 
 	@Test
-	public void testVerticalWithinDeadBand() {
-		testXY(0, 0.1, 0, 0);
-		testXY(0, -0.1, 0, 0);
+	public void testQuadrantsJustRight() {
+		double xIn = 0.70710678118;
+		double yIn = 0.70710678118;
+		double xOut = 0.707;
+		double yOut = 0.707;
+		testXY(xIn, yIn, xOut, yOut); // q1
+		testXY(-xIn, yIn, -xOut, yOut); // q2
+		testXY(-xIn, -yIn, -xOut, -yOut); // q3
+		testXY(xIn, -yIn, xOut, -yOut); // q4
 	}
 
 	@Test
@@ -45,6 +57,24 @@ public class CircularDeadBandTest {
 		testXY(-xIn, yIn, -xOut, yOut); // q2
 		testXY(-xIn, -yIn, -xOut, -yOut); // q3
 		testXY(xIn, -yIn, xOut, -yOut); // q4
+	}
+
+	@Test
+	public void testVertical() {
+		testXY(0, 0.5, 0, 0.264);
+		testXY(0, -0.5, 0, -0.264);
+	}
+
+	@Test
+	public void testVerticalJustRight() {
+		testXY(0, 1.0, 0, 1.0);
+		testXY(0, -1.0, 0, -1.0);
+	}
+
+	@Test
+	public void testVerticalWithinDeadBand() {
+		testXY(0, 0.1, 0, 0);
+		testXY(0, -0.1, 0, 0);
 	}
 
 	public void testXY(double xInput, double yInput, double expectedX, double expectedY) {
